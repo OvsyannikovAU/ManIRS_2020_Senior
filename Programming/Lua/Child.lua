d8e8ef09e154298b2770bd410b21ab1e729aca33
OvsyannikovAU <ovsyannikov.a.u@gmail.com>
2020-06-15 16:39:33 +0300

    Models update
    Обновление моделей под удобную проверку на программе-судье: вынос скриптов в отдельные файлы, добавление свойства disconnect для удаленного клиента

---
+++ Programming/Lua/Child.lua
@@ -0,0 +1,21 @@
+function sysCall_threadmain()
+    --require "Programming/Lua/RobotFunctions"
+    --roboInit()
+    -- Put some initialization code here
+    
+
+    -- Put your main action here, e.g.:
+    
+    
+    
+    
+    -- sim.setThreadAutomaticSwitch(false) -- disable automatic thread switches, wait in current step
+    -- Do some one-step actions
+    -- sim.switchThread() -- resume in next simulation step
+end
+
+function sysCall_cleanup()
+    -- Put some clean-up code here
+end
+
+-- See the user manual or the available code snippets for additional callback functions and details
